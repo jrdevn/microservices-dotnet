f664b035dc724a41972a6d3cf573d5b75a028587
jhonatajr <48875248+jrdevn@users.noreply.github.com>
2022-05-26 00:21:10 -0300

    feature(configuration): init configuration db and model product

---
+++ Shopping.ProductAPI/Program.cs
@@ -0,0 +1,34 @@
+using Microsoft.EntityFrameworkCore;
+using Shopping.ProductAPI.Model.Context;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+// database MySql
+builder.Services.AddControllers();
+var connection = builder.Configuration["MySQLConnection:MySQLConnectionString"];
+
+builder.Services.AddDbContext<MySQLContext>(options => options.
+                UseMySql(connection, 
+                        new MySqlServerVersion(
+                            new Version(8, 0, 29))));
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
