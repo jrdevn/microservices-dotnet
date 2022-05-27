6956a3419b73caf7a29802dee60c2296c9b08f3c
jhonatajr <48875248+jrdevn@users.noreply.github.com>
2022-05-27 00:37:17 -0300

    feature(product-controllers): adicionado os metodos get para acesso da api.

---
+++ Shopping.ProductAPI/Controllers/ProductController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shopping.ProductAPI.Data.ValueObjects;
+using Shopping.ProductAPI.Repository.Interface;
+
+namespace Shopping.ProductAPI.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private IProductRepository _productRepository;
+
+        public ProductController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository ?? throw new
+                ArgumentNullException(nameof(productRepository));
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ProductVO>> FindById(long id)
+        {
+            var product = await _productRepository.FindById(id);
+            if (product == null) return NotFound("Produto não encontrado!");
+            return Ok(product);
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductVO>>> FindAll()
+        {
+            var products = await _productRepository.FindAll();
+            return Ok(products);
+        }
+    }
+}
